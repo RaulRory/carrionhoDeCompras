24c652939f68b4c890f45ebe88fd2a0aba398f06
Raul Rory <raul.pereira@kroton.com.br>
2022-04-30 02:42:06 -0300

    add getter and setter

--- src/produto/Produto.ts
+++ src/produto/Produto.ts
@@ -1,11 +1,35 @@
 export class Produto {
-  codigo: number;
-  descricao: string;
-  preco: number;
+  private _codigo: number;
+  private _descricao: string;
+  private _preco: number;
 
   constructor(codigo: number, descricao: string, preco: number) {
-    this.codigo = codigo;
-    this.descricao = descricao;
-    this.preco = preco;
+    this._codigo = codigo;
+    this._descricao = descricao;
+    this._preco = preco;
+  }
+
+  public get codigo(): number {
+    return this._codigo;
+  }
+
+  public set codigo(codigo: number) {
+    this._codigo = codigo;
+  }
+
+  public get descricao(): string {
+    return this._descricao;
+  }
+
+  public set descricao(descricao: string) {
+    this._descricao = descricao;
+  }
+
+  public get preco(): number {
+    return this._preco;
+  }
+
+  public set preco(preco: number) {
+    this._preco = preco;
   }
 }
